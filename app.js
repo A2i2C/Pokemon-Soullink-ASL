c4bf17b4336b5b4574139b8d10c10b5ecb1a9522
Aziz <carducciaziz@gmail.com>
2026-03-21 17:17:52 +0100

    feat: refactor event listeners

--- app.js
+++ app.js
@@ -2,8 +2,9 @@
 const SUPABASE_URL = "https://sbteykcuvbjlcghbtcqt.supabase.co";
 const SUPABASE_KEY = "sb_publishable_pYVsZk-cxgbJJPVvmi7Szg_vgQfNNHj";
 const _supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+const mainScope = document.getElementById("main-scope"); // Für den Listener
+const trackerList = document.getElementById("trackerList"); // Für loadRoutes
 let showDead = false; // Steuert die Sichtbarkeit von "toten" Routen
-const list = document.getElementById("trackerList");
 
 // --- FUNKTIONEN ---
 
@@ -24,27 +25,13 @@ const channel = _supabase
   )
   .subscribe();
 
-    list.addEventListener("click", function (e) {
-    const routeId = e.target.closest(".route-wrapper").dataset.id; 
-    if (e.target.classList.contains("btn-update")) {
-      const player = e.target.dataset.player;
-      updatePoke(routeId, player);
-    }
-
-    if (e.target.classList.contains("btn-death")) {
-      const currentStatus = e.target.textContent.includes("REVIVE") ? "dead" : "alive"; // Wenn der Button "revive" anzeigt, ist die Route aktuell "dead" deswegen setzen wir den Status auf "alive" und umgekehrt
-      toggleStatus(routeId, currentStatus);
-    }
-  });
-
 async function loadRoutes() {
+  trackerList.innerHTML = ""; // Leert die aktuelle Liste, bevor neue Daten geladen werden
+
   const { data, error } = await _supabase
     .from("Pokemon")
     .select("*")
-    .order("id", { ascending: true }); // Sortiert nach der ID (1, 2, 3...)
-
-   list.innerHTML = "";
-
+    .order("id", { ascending: true });
 
   if (error) {
     console.error(error);
@@ -57,28 +44,30 @@ async function loadRoutes() {
     const routeTemplate = document.getElementById("routeTemplate");
     const routeTemplateClone = routeTemplate.content.cloneNode(true);
 
-    const route = routeTemplateClone.querySelector(".route-wrapper");
+    const route = routeTemplateClone.querySelector(".route-grid");
     route.dataset.id = row.id;
     routeTemplateClone.querySelector(".route-name").textContent = row.route;
-    if(row.status === "dead") route.classList.add("dead");
+    if (row.status === "dead") route.classList.add("dead");
 
     playerList.forEach((player) => {
-    const playerTemplate = document.getElementById("playerTemplate");
-    const playerTemplateClone = playerTemplate.content.cloneNode(true);
+      const playerTemplate = document.getElementById("playerTemplate");
+      const playerTemplateClone = playerTemplate.content.cloneNode(true);
 
-    const playersContainer = routeTemplateClone.querySelector(".players-container");
+      const playersContainer = routeTemplateClone.querySelector(".players-container");
 
-    playerTemplateClone.querySelector(".poke-name").textContent = row[player] || "---";
+      playerTemplateClone.querySelector(".poke-name").textContent = row[player] || "---";
 
-    const btnPokemonUpdate = playerTemplateClone.querySelector(".btn-update");
-    btnPokemonUpdate.dataset.player = player;
+      const btnPokemonUpdate = playerTemplateClone.querySelector(".btn-update");
+      btnPokemonUpdate.dataset.player = player;
 
-    playersContainer.appendChild(playerTemplateClone);
+      playersContainer.appendChild(playerTemplateClone);
     });
 
     const deathBtn = routeTemplateClone.querySelector(".btn-death");
     deathBtn.textContent = row.status === "alive" ? "MARK DEAD 💀" : "REVIVE 😇";
-    list.appendChild(routeTemplateClone);
+    
+    if (showDead === false && row.status === "dead") return;
+    trackerList.appendChild(routeTemplateClone);
   });
 }
 
@@ -123,13 +112,36 @@ async function toggleStatus(id, currentStatus) {
   if (error) alert(error.message);
 }
 
-
-
 // --- EVENT LISTENER ---
-document.getElementById("addRouteBtn").addEventListener("click", createRoute);
-document.getElementById("toggleDead").addEventListener("click", function() {
-  showDead = this.checked;
-  loadRoutes();
+mainScope.addEventListener("click", function (e) {
+  // Globale Funktionen
+
+  if (e.target.id === "toggleDead") {
+    showDead = e.target.checked;
+    loadRoutes(); // Neu laden, da keine Echtzeit-Updates für das Filtern existieren
+    return;
+  }
+  if (e.target.id === "addRouteBtn") {
+    createRoute();
+    return;
+  }
+
+  // Routen-Logik
+  const wrapper = e.target.closest(".route-wrapper");
+
+  const routeId = wrapper.dataset.id;
+
+  if (e.target.classList.contains("btn-update")) {
+    const player = e.target.dataset.player;
+    updatePoke(routeId, player);
+  }
+
+  if (e.target.classList.contains("btn-death")) {
+    const currentStatus = e.target.textContent.includes("REVIVE")
+      ? "dead"
+      : "alive"; // Wenn der Button "revive" anzeigt, ist die Route aktuell "dead" deswegen setzen wir den Status auf "alive" und umgekehrt
+    toggleStatus(routeId, currentStatus);
+  }
 });
 
 // Start
